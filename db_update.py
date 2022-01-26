18b395ab75a79cc4e6c4d8b320faaad51eaf190f
Bo Bramer <49931047+wuguishifu@users.noreply.github.com>
2022-01-26 12:48:53 -0800

    Removed card audacity.

--- db_update.py
+++ db_update.py
@@ -8,10 +8,10 @@ with open('mujank_db.json') as json_file:
     for user in list(users):
         user_data = users[user]
         if 'c_id_0002' in user_data['cards']:
-            num = user_data['cards']['c_id_0002']
-            del user_data['cards']['c_id_0002']
+            num = user_data['cards']['c_id_0475']
+            del user_data['cards']['c_id_0475']
             for i in range(num):
-                database.add_card(user, 'c_id_0001')
+                database.add_card(user, 'c_id_0474')
 
 with open('mujank_db.json', 'w') as json_file:
     json.dump(data, json_file, indent=4)
