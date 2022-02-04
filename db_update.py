4e1a8092747c8d7037b0d0eb427616166fb6e20e
Bo Bramer <49931047+wuguishifu@users.noreply.github.com>
2022-02-04 12:38:02 -0800

    db update

--- db_update.py
+++ db_update.py
@@ -4,7 +4,8 @@ with open('mujank_db.json') as json_file:
     data = json.load(json_file)
     users = data['users']
     for user in list(users):
-        data['users'][user]['inventory']['coins'] += 30
+        if data['users'][user]['inventory']['coins'] == 30:
+            data['users'][user]['inventory']['coins'] -= 30
 
 with open('mujank_db.json', 'w') as json_file:
     json.dump(data, json_file, indent=4)
