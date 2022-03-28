4e30190702651495fb61ada803181f4c61c5ff30
Matthew Grosman <grosmanmatt@gmail.com>
2022-03-27 19:54:55 -0700

    Added in people's birthdays and refined dictionary grabbing code

--- birthdays/birthday_dictionary.py
+++ birthdays/birthday_dictionary.py
@@ -1,6 +1,5 @@
 import json
 from birthdays import birthday_constants
-from collections import defaultdict
 
 
 def get_dictionary() -> dict:
@@ -12,17 +11,9 @@ def get_dictionary() -> dict:
                 following format:
                     {birthDate1: [userID1], birthDate2: [userID2, userID3,...],....}
     """
-    # A dictionary that contains all birthdays in Jank in the following format:
-    #   {birthDate1: [userID1], birthDate2: [userID2, userID3,...],....}
-    birthdays = defaultdict(list)
 
-    # Load in the JSON data about everyone's birthday
     with open(birthday_constants.BIRTHDAY_JSON_FILE) as json_file:
-        data = json.load(json_file)
+        return json.load(json_file)[birthday_constants.USERS_FIELD_NAME]
 
-        # Place each person into the "birthdays" dictionary
-        for birthday, user_id in data[birthday_constants.USERS_FIELD_NAME].items():
-            birthdays[birthday].append(user_id)
 
-    return dict(birthdays)
 
